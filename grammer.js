9fa477862ccd61c304dd91176f2f8126610a17eb
hyennin <chm050202@gmail.com>
2023-03-15 10:07:00 +0900

    befpore 매개변수가 하나일 때

--- grammer.js
+++ grammer.js
@@ -5,19 +5,26 @@
 // console.log(a);
 // // a = 10; // 에러, 상수는 변경 X
 
-// 함수(function)
-function add(n1, n2) {
-  return n1 + n2;
+// // 함수(function)
+// function add(n1, n2) {
+//   return n1 + n2;
+// }
+
+// result = add(1, 2)
+// console.log(result); // 2
+
+// plus = add; // plus도 add의 역할을 할 수 있음
+// result = plus(3, 4);
+// console.log(result); // 7
+
+// sub = (n1, n2) =>  n1 - n2;
+
+// result = sub(3, 2);
+// console.log(result) // 1
+
+square = (num) => {
+  return num * num;
 }
 
-result = add(1, 2)
-console.log(result); // 2
-
-plus = add; // plus도 add의 역할을 할 수 있음
-result = plus(3, 4);
-console.log(result); // 7
-
-sub = (n1, n2) =>  n1 - n2;
-
-result = sub(3, 2);
-console.log(result) // 1
+result = square(3);
+console.log(result); // 9
